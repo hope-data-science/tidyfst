5387dbc0160ae198f03c0f86c9cc89bf7ae36ede
Hope <32947502+hope-data-science@users.noreply.github.com>
2020-03-01 19:29:28 +0800

    Add files via upload

---
+++ R/complete.R
@@ -0,0 +1,61 @@
+
+#' @title Complete a data frame with missing combinations of data
+#' @description Turns implicit missing values into explicit missing values.
+#' Analogous function for \code{complete} function in \pkg{tidyr}.
+#' @param data data.frame
+#' @param ... Specification of columns to expand.The selection of columns is
+#' supported by the flexible \code{\link[tidyfst]{select_dt}}.
+#' To find all unique combinations of provided columns, including those not found in the data,
+#' supply each variable as a separate argument. But the two modes (select the
+#' needed columns and fill outside values) could not be mixed,
+#' find more details in examples.
+#' @param fill Atomic value to fill into the missing cell, default uses \code{NA}.
+#' @details If you supply fill, these values will also replace existing explicit missing values in the data set.
+#' @seealso \code{\link[tidyr]{complete}}
+#' @return data.table
+#' @examples
+#' df <- data.table(
+#'   group = c(1:2, 1),
+#'   item_id = c(1:2, 2),
+#'   item_name = c("a", "b", "b"),
+#'   value1 = 1:3,
+#'   value2 = 4:6
+#' )
+#'
+#' df %>% complete_dt(item_id,item_name)
+#' df %>% complete_dt(item_id,item_name,fill = 0)
+#' df %>% complete_dt("item")
+#' df %>% complete_dt(item_id=1:3)
+#' df %>% complete_dt(item_id=1:3,group=1:3,item_name=c("a","b","c"))
+
+#' @export
+complete_dt = function(data,...,fill = NA){
+  dt = as_dt(data)
+
+  if(
+    substitute(list(...)) %>%
+     deparse() %>%
+     str_detect("=")
+  ) {
+    data.table(...) %>%
+      expand.grid() %>%
+      as.data.table(key=names(.)) %>%
+      unique() %>%
+      merge(setorder(dt),all = TRUE) %>%
+      replace_na_dt(to=fill)
+  }else
+    setorder(dt)%>%
+    select_dt(...) %>%
+    unique() %>%
+    expand.grid() %>%
+    as.data.table(key = names(.)) %>%
+    merge(dt,all = TRUE) %>%
+    replace_na_dt(to=fill)
+}
+
+
+
+
+
+
+
---
+++ R/dummy_dt.R
@@ -0,0 +1,65 @@
+
+#' @title Fast creation of dummy variables
+#' @description Quickly create dummy (binary) columns from character and factor type columns in the inputted data (and numeric columns if specified.)
+#' This function is useful for statistical analysis when you want binary columns rather than character columns.
+#' @param data data.frame
+#' @param ... Columns you want to create dummy variables from.
+#' Very flexible, find in the examples.
+#' @param longname logical. Should the output column labeled with the
+#' original column name? Default uses \code{TRUE}.
+#' @return data.table
+#' @details If no columns provided, will return the original data frame.
+#' @details This function is inspired by \pkg{fastDummies} package, but provides
+#' simple and precise usage, whereas \code{fastDummies::dummy_cols} provides more
+#' features for statistical usage.
+#' @seealso \code{\link[fastDummies]{dummy_cols}}
+#' @examples
+#' iris %>% dummy_dt(Species)
+#' iris %>% dummy_dt(Species,longname = FALSE)
+#'
+#' mtcars %>% head() %>% dummy_dt(vs,am)
+#' mtcars %>% head() %>% dummy_dt("cyl|gear")
+
+#' @export
+dummy_dt = function(data,...,longname = TRUE){
+  dt = as_dt(data)
+  if((substitute(list(...)) %>% deparse())=="list()")
+    warning("No columns provided, return the orginal data.")
+  else{
+    dt %>%
+      select_dt(...) %>%
+      names() -> col_names
+    for(i in col_names){
+      dummy_col(dt,i,longname = longname) -> dt
+    }
+  }
+  dt
+}
+
+
+dummy_col = function(data,col_name,longname){
+  dt = as_dt(data)
+  #substitute(col) %>% deparse() -> col_name
+  dt[, `:=`(one_=1,id_=1:.N) ]
+
+  if(longname){
+    dt[[col_name]] %>% unique() %>% as.character() -> old_names
+    str_c(col_name,old_names,sep="_") -> new_names
+    str_glue("dt=dcast(dt,...~{col_name},value.var = 'one_',fill = 0)") -> to_eval
+    eval(parse(text = to_eval))
+    setnames(dt,old_names,new_names)
+  }else{
+    str_glue("dt=dcast(dt,...~{col_name},value.var = 'one_',fill = 0)") -> to_eval
+    eval(parse(text = to_eval))
+  }
+  dt[,id_:=NULL][]
+}
+
+
+
+
+
+
+
+
+
